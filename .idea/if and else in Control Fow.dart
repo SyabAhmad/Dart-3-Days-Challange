3438ecbd55f414b8d418bdad0c92d9b5b20d374f
SyabAhmad <syabblogger@gmail.com>
2023-03-10 14:43:08 +0500

    for loop, if else, else if and switch cases in Dart

---
+++ .idea/if and else in Control Fow.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  print("Controle Flow in Dart");
+  int age = 18;
+  if (age != 18)
+    {
+      print("You Must be 18 to continue...");
+    }
+  else{
+    print("Account successfully created");
+  }
+}
---
+++ .idea/switch case in Dart control flows.dart
@@ -0,0 +1,22 @@
+void main()
+{
+  // switch cases in Dart
+  String operator = '+';
+  switch(operator ){
+    case '+':
+      print(2+4);
+      break;
+    case '-':
+      print(4-2);
+      break;
+    case '*':
+      print(4*4);
+      break;
+    case '/':
+      print(10/2);
+      break;
+    default:
+      print("Invalid Arguments");
+      break;
+  }
+}
